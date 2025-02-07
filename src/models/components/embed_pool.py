a54c588583e551ba9ad6f8a02503c79bc25a9c61
Ben Hayes <b.j.hayes@qmul.ac.uk>
2025-02-07 16:52:35 +0000

    pos encs for embedding

--- src/models/components/embed_pool.py
+++ src/models/components/embed_pool.py
@@ -3,26 +3,24 @@ import math
 import torch
 import torch.nn as nn
 
+from src.models.components.transformer import SinusoidalEncoding
+
 
 class EmbeddingPool(nn.Module):
     def __init__(self, embed_dim: int, d_model: int, num_heads: int):
         super().__init__()
 
-        self.ffn = nn.Sequential(
-            nn.LayerNorm(embed_dim),
-            nn.Linear(embed_dim, d_model),
-            nn.GELU(),
-            nn.Linear(d_model, d_model),
+        self.attn = nn.MultiheadAttention(
+            d_model, num_heads, kdim=embed_dim, vdim=embed_dim, batch_first=True
         )
-        self.resid = nn.Linear(embed_dim, d_model, False)
-
-        self.attn = nn.MultiheadAttention(d_model, num_heads, batch_first=True)
-        self.query = nn.Parameter(torch.randn(1, 1, d_model) / math.sqrt(d_model))
+        self.query = nn.Parameter(torch.randn(1, 1, d_model))
+        self.positional_encoding = nn.Parameter(torch.zeros(1, 1, d_model))
 
     def forward(self, embed: torch.Tensor):
         embed = embed.permute(0, 2, 1)
-        embed = self.ffn(embed) + self.resid(embed)
-        query = self.query.repeat(embed.shape[0], 1, 1)
-        output, _ = self.attn(query, embed, embed)
 
-        return (output + query).squeeze(1)
+        embed = embed + self.positional_encoding
+        query = self.query.repeat(embed.shape[0], 1, 1)
+        embed, _ = self.attn(query=query, key=embed, value=embed)
+
+        return embed.squeeze(1)
