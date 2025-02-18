e179987ff84699f4cc6bd668591fa091fce96e25
Ben Hayes <b.j.hayes@qmul.ac.uk>
2025-02-18 11:31:22 +0000

    add logs

--- scripts/model_from_wandb_repl.py
+++ scripts/model_from_wandb_repl.py
@@ -1,10 +1,11 @@
 from pathlib import Path
-from typing import Literal
+from typing import Literal, Tuple
 
 import click
 import hydra
 import torch
 from IPython import embed
+from loguru import logger
 from omegaconf import DictConfig, OmegaConf
 
 
@@ -14,9 +15,12 @@ def wandb_dir_to_ckpt_and_hparams(
     log_dir = wandb_dir.parent
     hparam_file = log_dir / "csv" / "hparams.yaml"
     ckpt_dir = log_dir / "checkpoints"
+
     if ckpt_type == "last":
+        logger.info(f"Using last checkpoint for {log_dir(log_dir)}")
         ckpt_file = ckpt_dir / "last.ckpt"
     elif ckpt_type == "best":
+        logger.info(f"Using best checkpoint for {log_dir(log_dir)}")
         ckpt_files = ckpt_dir.glob("epoch*.ckpt")
 
         # most recent file
@@ -27,6 +31,7 @@ def wandb_dir_to_ckpt_and_hparams(
 
 
 def get_state_dict(ckpt_file: Path, map_location: str = "cuda") -> dict:
+    logger.info(f"Loading checkpoint from {ckpt_file}")
     ckpt = torch.load(ckpt_file, map_location=map_location, weight_only=False)
     state_dict = ckpt["state_dict"]
     return state_dict
@@ -35,6 +40,9 @@ def get_state_dict(ckpt_file: Path, map_location: str = "cuda") -> dict:
 def instantiate_model(
     model_cfg: DictConfig, ckpt_file: Path, map_location: str = "cuda"
 ) -> torch.nn.Module:
+    logger.info(f"Instantiating model from {ckpt_file} with config:")
+    logger.info(OmegaConf.to_yaml(model_cfg))
+
     state_dict = get_state_dict(ckpt_file, map_location=map_location)
 
     model = hydra.utils.instantiate(model_cfg)
@@ -46,6 +54,8 @@ def instantiate_model(
 
 
 def instantiate_datamodule(data_cfg: DictConfig):
+    logger.info("Instantiating datamodule with config:")
+    logger.info(OmegaConf.to_yaml(data_cfg))
     return hydra.utils.instantiate(data_cfg)
 
 
@@ -62,6 +72,7 @@ def main(
 ):
     log_dir = Path(log_dir)
     possible_wandb_dirs = log_dir.glob(f"**/*{wandb_id}/")
+    logger.info(f"Found {len(possible_wandb_dirs)} log dirs matching wandb id")
 
     ckpts_and_hparams = map(
         lambda x: wandb_dir_to_ckpt_and_hparams(x, ckpt_type), possible_wandb_dirs
@@ -82,6 +93,7 @@ def main(
     model = instantiate_model(cfg.model, ckpt_file, device)
     datamodule = instantiate_datamodule(cfg.data)
 
+    logger.info("Starting REPL...")
     embed(user_ns={"model": model, "datamodule": datamodule, "cfg": cfg})
 
 
