d759301698e17248cc0b183022d669ec00cbadca
Ben Hayes <b.j.hayes@qmul.ac.uk>
2025-03-05 13:26:08 +0000

    update job submission

---
+++ jobs/train/surge/train.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#$ -l h_rt=240:0:0
+#$ -l gpu=1
+#$ -cwd
+#$ -j y
+#$ -o qlogs/
+#$ -e qlogs/
+
+#$ -l rocky
+
+#$ -l cluster=andrena
+#$ -l h_vmem=7.5G
+#$ -pe smp 12
+
+# -l node_type=rdg
+# -l gpuhighmem
+# -l h_vmem=20G
+# -pe smp 12
+
+#$ -t 1-6
+
+EXPERIMENT=$(sed -n "${SGE_TASK_ID}p" jobs/train/surge/experiments.txt)
+
+rm -rf ~/.triton/cache
+mamba activate perm
+module load gcc
+python src/train.py experiment=$EXPERIMENT \
+  seed=999
